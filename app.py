a2a069b8a10e882a0f150e3f94a0accca32c140d
Jananporn Banjob <jananporn.banjob.gamil.com>
2025-02-18 04:20:32 +0700

    add edit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(300), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Bangkok')))
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(pytz.timezone('Asia/Bangkok')))
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
